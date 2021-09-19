92c7ce23ad1ef82871bd14a0a7f96dd2556c2b73
psshmart <65891171+psshmart@users.noreply.github.com>
2021-09-19 18:12:27 +0300

    Added function for dividing

--- homework_1/homework_1/ViewController.swift
+++ homework_1/homework_1/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let dividend = 10
+    let divider = 2
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let result = divideNumbers(dividend: dividend, divider: divider)
+        print("\(result)")
     }
-
-
+    
+    private func divideNumbers(dividend: Int, divider: Int) -> Int {
+        dividend / divider
+    }
 }
 

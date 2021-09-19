e28a0b834781f2a33c0fcdee76a154534a509bcf
psshmart <65891171+psshmart@users.noreply.github.com>
2021-09-19 18:05:59 +0300

    Added label for sum displaying

--- homework_1/homework_1/ViewController.swift
+++ homework_1/homework_1/ViewController.swift
@@ -12,11 +12,14 @@ class ViewController: UIViewController {
     let firstNumber = 5
     let secondNumber = 10
 
+    @IBOutlet weak var sumLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         let total = sumUp(first: firstNumber, second: secondNumber)
         print(total)
+        sumLabel.text = "Sum = \(total)"
     }
     
     private func sumUp(first: Int, second: Int) -> Int {

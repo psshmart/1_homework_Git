67dc8186315581d9520c8d8a31db5d27abbe37fe
psshmart <65891171+psshmart@users.noreply.github.com>
2021-09-19 18:29:47 +0300

    Added UI for dividing

--- homework_1/homework_1/ViewController.swift
+++ homework_1/homework_1/ViewController.swift
@@ -8,17 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-    let dividend = 10
-    let divider = 2
+    var dividend = 10
+    var divider = 2
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var textFieldForDividend: UITextField!
+    @IBOutlet weak var textFielfForDivider: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        let result = divideNumbers(dividend: dividend, divider: divider)
-        print("\(result)")
     }
     
     private func divideNumbers(dividend: Int, divider: Int) -> Int {
         dividend / divider
+    }
+    
+    @IBAction func divideAction(_ sender: Any) {
+        guard let dividend = textFieldForDividend.text else { return }
+        guard let divider = textFielfForDivider.text else { return }
+        self.dividend = Int(dividend) ?? 0
+        self.divider = Int(divider) ?? 1
+        let result = divideNumbers(dividend: self.dividend, divider: self.divider)
+        resultLabel.text = "\(result)"
     }
 }
 

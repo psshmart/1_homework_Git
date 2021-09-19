647629067d94133c0c8c93afcac4e6cfa1382106
psshmart <65891171+psshmart@users.noreply.github.com>
2021-09-19 17:56:31 +0300

    init project

---
+++ homework_1/homework_1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework_1
+//
+//  Created by Svetlana Safonova on 19.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
